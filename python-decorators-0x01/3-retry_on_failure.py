eb2e759ae7f9365f69bf1cef324239be530b71b2
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-07-09 17:04:57 +0100

    retry logic added

---
+++ python-decorators-0x01/3-retry_on_failure.py
@@ -0,0 +1,87 @@
+import time
+import sqlite3
+import functools
+from typing import Callable, Any
+
+def with_db_connection(func: Callable) -> Callable:
+    """
+    A decorator that opens a SQLite database connection and closes it automatically.
+
+    Args:
+        func: The function to be passed to the decorator.
+
+    Returns:
+        Callable: The wrapped function with an active database connection passed as an argument.
+    """
+    @functools.wraps(func)
+    def wrapper_connect(*args: Any, **kwargs: Any) -> Any:
+        conn = sqlite3.connect('users.db')
+        try:
+            result = func(conn=conn, *args, **kwargs)
+        finally:
+            conn.close()
+        return result
+    return wrapper_connect
+
+def retry_on_failure(retries: int, delay: int) -> Callable:
+    """
+
+    Args:
+        retries: Maximum number of retry attempts before giving up.
+        delay: Delay in seconds between attempts.
+
+    Returns:
+        Callable: The wrapped function with retry logic.
+    """
+    def transactional(func: Callable) -> Callable:
+        """
+            A decorator that manages database transactions, committing on success and rolling back on failure.
+            Retries the function up to 'retries' times in case of exceptions.
+
+            Args:
+                func: Function to be passed to the decorator.
+
+            Returns:
+                Callable: The wrapped function with transaction and retry logic.
+
+        """
+
+        @functools.wraps(func)
+        def wrapper(conn: sqlite3.Connection, *args: Any, **kwargs: Any) -> Any:
+            for attempt in range(retries + 1):
+                try:
+                    result = func(conn, *args, **kwargs)
+                    conn.commit()
+                    return result
+                except Exception as e:
+                    conn.rollback()
+                    if attempt == retries:
+                        print(f"Max retries reached persistent errors in retry: {e}")
+                        raise
+                    print(f"Error processing request retrying...")
+                    time.sleep(delay)
+
+        return wrapper
+    return transactional
+
+
+@with_db_connection
+@retry_on_failure(retries=3, delay=1)
+def fetch_users_with_retry(conn: sqlite3.Connection) -> list | None:
+    """
+        Fetch all users from the 'users' table with automatic transaction handling and retry on failure.
+
+        Args:
+            conn: SQLite connection object provided by the decorators.
+
+        Returns:
+            A list of tuples representing user rows if successful, otherwise None.
+        """
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users")
+    return cursor.fetchall()
+
+#### attempt to fetch users with automatic retry on failure
+
+users = fetch_users_with_retry()
+print(users)
