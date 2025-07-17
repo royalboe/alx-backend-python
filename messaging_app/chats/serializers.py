5bee31ffde12c5ccdc2ef34966a6c6dc78c76ee3
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-07-17 19:13:28 +0100

    added models

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,12 +1,72 @@
 from rest_framework import serializers
-from .models import Message, Conversation
+from .models import Message, Conversation, User
 
-class MessageSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
+    confirm_password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True)
+
     class Meta:
-        model = Message
-        fields = '__all__'
+        model = User
+        fields = [
+            "user_id",
+            "email",
+            "password",
+            "first_name",
+            "last_name",
+            "phone_number",
+            "profile_image",
+        ]
+        read_only_fields = ["user_id"]
+
+    def create(self, validated_data):
+        validated_data.pop("confirm_password")  # Remove extra field
+        user = User.objects.create_user(
+            email=validated_data["email"],
+            password=validated_data["password"],
+            first_name=validated_data.get("first_name", ""),
+            last_name=validated_data.get("last_name", ""),
+            phone_number=validated_data.get("phone_number", ""),
+            profile_image=validated_data.get("profile_image", None),
+        )
+        return user
+
+    def validate(self, data):
+        if data.get("password") != data.get("confirm_password"):
+            raise serializers.ValidationError("Passwords do not match.")
+        return data
+
 
 class ConversationSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(many=True, read_only=True)
+    participant_ids = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(), many=True, write_only=True, source='participants'
+    )
     class Meta:
         model = Conversation
-        fields = ['messages', 'participants', 'created_at', 'conversation_id']
+        fields = ['conversation_id', 'participants', 'created_at', 'participant_ids']
+        read_only_fields = ["created_at"]
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    sender = UserSerializer(read_only=True)
+    sender_id = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(), write_only=True, source="sender"
+    )
+    conversation = serializers.SlugRelatedField(
+        slug_field="conversation_id",
+        queryset=Conversation.objects.all()
+    )
+
+    class Meta:
+        model = Message
+        fields = [
+            "message_id",
+            "message_body",
+            "conversation",
+            "sender",
+            "sender_id",
+            "sent_at",
+        ]
+        read_only_fields = ["sent_at"]
+
+
