286b9b0aed9181c6163cd96dbe15887013f33222
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-07-17 14:40:36 +0100

    added seriazers

---
+++ messaging_app/chats/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Message, Conversation
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = '__all__'
+
+class ConversationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Conversation
+        fields = ['messages', 'participants', 'created_at', 'conversation_id']
