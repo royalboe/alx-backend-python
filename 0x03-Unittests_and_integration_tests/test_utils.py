acea440165ccd48dac46b8350cb11ed8a3b0b79f
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-07-20 13:14:03 +0100

    checks for erros

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -20,6 +20,17 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """ method to test that the method returns what it is supposed to """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    """  to test that a KeyError is raised for the following inputs """
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """ method to test that a KeyError is raised properly """
+        with self.assertRaises(KeyError) as error:
+            access_nested_map(nested_map, path)
+        self.assertEqual(error.exception.args[0], path[-1])
 # class TestAccessNestedMap(unittest.TestCase):
 #     """ TESTCASE """
 #     """ to test the function for following inputs """
