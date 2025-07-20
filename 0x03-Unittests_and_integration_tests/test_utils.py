adc89fa1a9a63712caa3103aca3edab9e23df7fe
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-07-20 13:22:54 +0100

    mocking requests.get instead

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """ Parameterize a unit test, Mock HTTP calls, Parameterize and patch """
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 from parameterized import parameterized
+import utils
 from utils import access_nested_map, get_json, memoize
 from typing import Any
 
@@ -31,29 +32,6 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as error:
             access_nested_map(nested_map, path)
         self.assertEqual(error.exception.args[0], path[-1])
-# class TestAccessNestedMap(unittest.TestCase):
-#     """ TESTCASE """
-#     """ to test the function for following inputs """
-#     @parameterized.expand([
-#         ({"a": 1}, ("a",), 1),
-#         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-#         ({"a": {"b": 2}}, ("a", "b"), 2),
-#     ])
-#     def test_access_nested_map(self, nested_map, path, answer):
-#         """ method to test that the method returns what it is supposed to """
-#         self.assertEqual(access_nested_map(nested_map, path), answer)
-#
-#     """  to test that a KeyError is raised for the following inputs """
-#     @parameterized.expand([
-#         ({}, ("a",)),
-#         ({"a": 1}, ("a", "b")),
-#     ])
-#     def test_access_nested_map_exception(self, nested_map, path):
-#         """ method to test that a KeyError is raised properly """
-#         with self.assertRaises(KeyError) as error:
-#             access_nested_map(nested_map, path)
-#         self.assertEqual(error.exception.args[0], path[-1])
-
 
 class TestGetJson(unittest.TestCase):
     """ TESTCASE """
@@ -62,10 +40,13 @@ class TestGetJson(unittest.TestCase):
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
     ])
-    @patch('test_utils.get_json')
+    @patch('utils.requests.get')
     def test_get_json(self, test_url, test_payload, mock_get):
         """ method to test that utils.get_json returns the expected result """
-        mock_get.return_value = test_payload
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
         result = get_json(test_url)
         self.assertEqual(result, test_payload)
 
