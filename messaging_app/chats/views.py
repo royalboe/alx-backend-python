3be02289fa77b98f7f07e6eb084109513491e55b
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-07-27 23:03:30 +0100

    for filtering

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,14 +1,35 @@
 from .models import User, Message, Conversation
 from .serializers import UserSerializer, MessageSerializer, ConversationSerializer
+from .permissions import IsOwner, IsParticipantOfConversation
+from .pagination import CustomMessagePagination
+from .filters import MessageFilter
+
 from rest_framework import viewsets, status, filters
 from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
 
 class MessageViewSet(viewsets.ModelViewSet):
     serializer_class = MessageSerializer
     queryset = Message.objects.all()
+    permission_classes = [IsAuthenticated, IsOwner, IsParticipantOfConversation]
+    pagination_class = CustomMessagePagination
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_class = MessageFilter
+    ordering_fields = ["sent_at"]
+    ordering = ["-sent_at"]
+
+    def get_queryset(self):
+         return Message.objects.filter(conversation__participants__user=self.request.user).distinct()
+
 
 class ConversationViewSet(viewsets.ModelViewSet):
     serializer_class = ConversationSerializer
     queryset = Conversation.objects.all()
+    permission_classes = [IsAuthenticated, IsParticipantOfConversation]
+
+    def get_queryset(self):
+        return Conversation.objects.filter(participants__user=self.request.user).distinct()
