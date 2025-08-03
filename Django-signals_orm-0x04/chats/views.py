023a5d9c21832d4f19cd81fa3f9fa7fae1de2a24
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-08-03 13:28:10 +0100

    added other files

--- Django-signals_orm-0x04/chats/views.py
+++ Django-signals_orm-0x04/chats/views.py
@@ -1,27 +1,11 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from .models import User, Message, Notification, MessageHistory
-from .serializers import UserSerializer, MessageHistorySerializer, MessageSerializer, NotificationSerializer
+from messaging.models import User, Message, Notification, MessageHistory
+from messaging.serializers import UserSerializer, MessageHistorySerializer, MessageSerializer, NotificationSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
 from django.views.decorators.cache import cache_page
 
-# Create your views here.
-class UserView(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-    # def destroy(self, request, *args, **kwargs):
-    #     instance = self.get_object()
-    #     self.perform_destroy(instance)
-    #     return Response(status=status.HTTP_204_NO_CONTENT)
-    @action(detail=False, methods=['delete'], permission_classes=[IsAuthenticated])
-    def delete_user(self, request):
-        """
-        Custom endpoint for a user to delete their own account.
-        """
-        request.user.delete()
-        return Response({"detail": "Your account has been deleted."}, status=status.HTTP_204_NO_CONTENT)
 
 class MessageView(viewsets.ModelViewSet):
     queryset = Message.objects.all()
---
+++ Django-signals_orm-0x04/messaging_app/messaging_app/settings.py
@@ -0,0 +1,2 @@
+
+CACHES = { 'default': { 'BACKEND': 'django.core.cache.backends.locmem.LocMemCache', 'LOCATION': 'unique-snowflake', } }
